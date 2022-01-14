acc7790e52a38c579f8ec44ae4ca009e8595c91b
Nick Santos <nick@tilt.dev>
2022-01-14 12:58:04 -0500

    ephctrl: run tilt

--- ephctrl/tilt-upper-entrypoint.sh
+++ ephctrl/tilt-upper-entrypoint.sh
@@ -1,4 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-env
-sleep 1000
+if [[ "$TILT_UPPER_PATH" == "" ]]; then
+    echo "Missing TILT_UPPER_PATH"
+    exit 1
+fi
+
+if [[ "$TILT_UPPER_REPO" == "" ]]; then
+    echo "Missing TILT_UPPER_REPO"
+    exit 1
+fi
+
+if [[ "$TILT_UPPER_BRANCH" == "" ]]; then
+    echo "Missing TILT_UPPER_BRANCH"
+    exit 1
+fi
+
+while ! docker ps
+do
+    echo waiting for Docker socket
+    sleep 2
+done
+
+set -euxo pipefail
+
+rm -fR ./src
+mkdir -p src
+git clone "$TILT_UPPER_REPO" src
+cd src
+git checkout "$TILT_UPPER_BRANCH"
+cd "$TILT_UPPER_PATH"
+
+ctlptl create registry ctlptl-registry --port=5000
+ctlptl create cluster kind --registry=ctlptl-registry
+tilt up
--- ephctrl/tilt-upper.dockerfile
+++ ephctrl/tilt-upper.dockerfile
@@ -1,5 +1,7 @@
 FROM tiltdev/tilt
 
+RUN apt update && apt install -y python3
 ADD tilt-upper-entrypoint.sh ./tilt-upper-entrypoint.sh
+ADD tilt-healthcheck.py ./tilt-healthcheck.py
 
 ENTRYPOINT ./tilt-upper-entrypoint.sh
